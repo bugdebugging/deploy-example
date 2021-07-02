2edcf7f6a3065bc874a7ed15ae8e7b18eafd8e98
bugdebugging <annyousung@gmail.com>
2021-07-02 23:18:36 +0900

    port change

---
+++ src/test/java/com/anny/jenkinsdemo/controller/UserControllerTest.java
@@ -0,0 +1,12 @@
+package com.anny.jenkinsdemo.controller;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+public class UserControllerTest {
+    @Test
+    void users(){
+
+    }
+}
